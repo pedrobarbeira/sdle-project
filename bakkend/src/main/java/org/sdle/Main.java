6875f892f0247883de5ae0f4d914a6a771b33b03
pbarbeira <pedro.barbeira95@gmail.com>
2023-10-13 10:20:31 +0100

    defined some models

---
+++ bakkend/src/main/java/org/sdle/Main.java
@@ -0,0 +1,12 @@
+package org.sdle;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, String> test = new HashMap<>();
+        test.put("hello", "hello");
+        test.put("hello", "goodbye");
+
+    }
+}
---
+++ bakkend/src/main/java/org/sdle/models/User.java
@@ -0,0 +1,4 @@
+package src.main.java.org.sdle.models;
+
+public class User {
+}
