9936db62016b39464574d793e11092eaed20d14e
pbarbeira <pedro.barbeira95@gmail.com>
2023-12-05 00:57:06 +0000

    stuff

--- backend/dataServerMvn/src/main/java/org/sdle/model/ShoppingItem.java
+++ backend/dataServerMvn/src/main/java/org/sdle/model/ShoppingItem.java
@@ -7,36 +7,35 @@ import java.util.UUID;
 public class ShoppingItem {
     String id;
     String name;
-
-    ItemType type;
+    boolean checked;
     int quantity;
 
     public ShoppingItem(){
-        this(UUID.randomUUID().toString(), "default item", ItemType.NUMERIC, 0);
+        this(UUID.randomUUID().toString(), "default item", false, 0);
     };
 
-    public ShoppingItem(String name, ItemType type, int quantity){
-        this(UUID.randomUUID().toString(), name, type, quantity);
+    public ShoppingItem(String name, boolean checked, int quantity){
+        this(UUID.randomUUID().toString(), name, checked, quantity);
     }
 
-    public ShoppingItem(String id, String name, ItemType type, int quantity){
+    public ShoppingItem(String id, String name, boolean checked, int quantity){
         this.id = id;
         this.name = name;
-        this.type = type;
+        this.checked = checked;
         this.quantity = quantity;
     }
 
     public String getId(){
-        return id;
+        return this.id;
     }
 
-    public String getName(){ return name;}
+    public String getName(){ return this.name;}
 
-    public String getType() {
-        return type.getType();
+    public boolean isChecked() {
+        return this.checked;
     }
 
-    public int getQuantity() { return quantity;}
+    public int getQuantity() { return this.quantity;}
 
     public void setQuantity(int newQuantity){
         this.quantity = newQuantity;
