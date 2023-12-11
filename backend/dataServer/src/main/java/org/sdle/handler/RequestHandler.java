a2a0f84d4a8923a4f985e72c576e4312c656b297
pbarbeira <pedro.barbeira95@gmail.com>
2023-12-11 02:03:27 +0000

    added presentation pdf

--- backend/dataServer/src/main/java/org/sdle/handler/RequestHandler.java
+++
@@ -1,9 +0,0 @@
-package org.sdle.handler;
-
-import org.sdle.api.Request;
-import org.sdle.api.Response;
-
-public interface RequestHandler {
-    Response handle(Request request);
-    Response buildResponse(Object body);
-}
